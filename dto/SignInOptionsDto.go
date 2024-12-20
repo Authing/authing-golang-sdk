fa6b68f07aa1df96ce6cc30c383138e2e1101900
lixueping <lixueping@authing.cn>
2024-12-20 23:21:07 +0800

    fix: AutoRegister 为 false 时会被忽略

--- dto/SignInOptionsDto.go
+++ dto/SignInOptionsDto.go
@@ -1,14 +1,12 @@
 package dto
 
-
-type SignInOptionsDto struct{
-    Scope  string `json:"scope,omitempty"`
-    ClientIp  string `json:"clientIp,omitempty"`
-    Context  interface{} `json:"context,omitempty"`
-    TenantId  string `json:"tenantId,omitempty"`
-    CustomData  interface{} `json:"customData,omitempty"`
-    AutoRegister  bool `json:"autoRegister,omitempty"`
-    CaptchaCode  string `json:"captchaCode,omitempty"`
-    PasswordEncryptType  string  `json:"passwordEncryptType,omitempty"`
+type SignInOptionsDto struct {
+	Scope               string      `json:"scope,omitempty"`
+	ClientIp            string      `json:"clientIp,omitempty"`
+	Context             interface{} `json:"context,omitempty"`
+	TenantId            string      `json:"tenantId,omitempty"`
+	CustomData          interface{} `json:"customData,omitempty"`
+	AutoRegister        bool        `json:"autoRegister"`
+	CaptchaCode         string      `json:"captchaCode,omitempty"`
+	PasswordEncryptType string      `json:"passwordEncryptType,omitempty"`
 }
-
